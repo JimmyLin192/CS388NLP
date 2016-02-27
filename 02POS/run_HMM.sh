831145f793c9ee089dec54c1bb74917da561c6fe
jimmylin192 <linxin@gmail.com>
2016-02-27 13:25:42 -0600

    run_HMM

---
+++ 02POS/run_HMM.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in `seq 1 10`; do
+    java -cp "class:lib/mallet-deps.jar" cc.mallet.fst.HMMSimpleTagger \
+        --train true --model-file atis3.HMM/$i.model \
+        --training-proportion 0.8 \
+        --random-seed $i \
+		--test lab datasets/atis3.mal 2> atis3.HMM/$i.log 
+	done
