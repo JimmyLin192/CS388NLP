90b335c865101ef3df8fac027cd1342292ec14a2
jimmylin192 <linxin@gmail.com>
2016-02-29 20:19:35 -0600

    run stuff

--- 02POS/run_atis3_CRF.sh
+++ 02POS/run_atis3_CRF.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in `seq 1 10`; do
-    (time java -cp "class:lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger \
-        --train true --model-file atis3.CRF.simple/$i.model \
-        --training-proportion 0.8 \
-        --random-seed $i \
-        --test lab datasets/atis3.mal 2> atis3.CRF.simple/$i.log ) &
-done
+mode="all"
+i=1
+
+#for i in `seq 1 10`; do
+    java -cp "class:lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger --train true --model-file atis3.CRF.$mode/$i.model --training-proportion 0.8 --random-seed $i --test lab datasets/atis3.mal.$mode )
+# 2> atis3.CRF.$mode/$i.log 
+#done
--- 02POS/run_wsj_CRF.sh
+++ 02POS/run_wsj_CRF.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-i=$1
-for i in `seq 1 10`; do
+mod=$1
+i=1
+#for i in `seq 1 10`; do
         (time java -cp "class:lib/mallet-deps.jar" cc.mallet.fst.SimpleTagger \
-        --train true --model-file wsj.CRF.simple/$i.model \
+        --train true --model-file wsj.CRF.$mod/$i.model \
         --training-proportion 0.5 \
         --random-seed $i \
-        --test lab datasets/wsj.mal 2> wsj.CRF.simple/$i.log) &
-    done
+        --test lab datasets/wsj.mal.$mod 2> wsj.CRF.$mod/$i.log) &
+#    done
